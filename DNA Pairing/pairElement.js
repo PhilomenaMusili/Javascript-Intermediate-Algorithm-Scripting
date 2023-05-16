c008cab1ff00d93d8eec791d1ccc63ae7202ecb0
PhilomenaMusili <105357843+PhilomenaMusili@users.noreply.github.com>
2023-05-16 11:48:06 +0300

    Update pairElement.js

--- DNA Pairing/pairElement.js
+++ DNA Pairing/pairElement.js
@@ -1,1 +1,17 @@
+function pairElement(str) {
+  // create object for pair lookup
+  const pairs = {
+    A: "T",
+    T: "A",
+    C: "G",
+    G: "C"
+  };
 
+  // map character to array of character and matching pair
+  return str
+    .split("")
+    .map(x => [x, pairs[x]]);
+}
+
+// test here
+pairElement("GCG");
