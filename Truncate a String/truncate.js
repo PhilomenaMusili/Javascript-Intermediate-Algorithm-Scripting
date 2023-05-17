b5b77e5cbca6e11d76b1ba2a77f56882f1689f2f
PhilomenaMusili <105357843+PhilomenaMusili@users.noreply.github.com>
2023-05-17 13:31:25 +0300

    Update truncate.js

--- Truncate a String/truncate.js
+++ Truncate a String/truncate.js
@@ -1,1 +1,10 @@
+function truncateString(str, num) {
+  // Clear out that junk in your trunk
+  if (str.length > num) {
+    return str.slice(0, num) + "...";
+  } else {
+    return str;
+  }
+}
 
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
