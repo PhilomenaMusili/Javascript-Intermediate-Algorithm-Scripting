9736367fda99b8b5040a3b0298511b7a59573763
PhilomenaMusili <105357843+PhilomenaMusili@users.noreply.github.com>
2023-06-14 20:55:28 +0300

    Update destroyer.js

--- Seek and Destroy/destroyer.js
+++ Seek and Destroy/destroyer.js
@@ -1,1 +1,17 @@
+function destroyer(arr) {
+  const valsToRemove = Object.values(arguments).slice(1);
+  const filteredArray = [];
 
+  for (let i = 0; i < arr.length; i++) {
+    let removeElement = false;
+    for (let j = 0; j < valsToRemove.length; j++) {
+      if (arr[i] === valsToRemove[j]) {
+        removeElement = true;
+      }
+    }
+    if (!removeElement) {
+      filteredArray.push(arr[i]);
+    }
+  }
+  return filteredArray;
+}
